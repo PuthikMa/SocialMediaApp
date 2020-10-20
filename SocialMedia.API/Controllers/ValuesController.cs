8ba3ff9eb9d3ed0f8e7a859c2bd499fbb610eab3
puthik ma <puthik.ma@gmail.com>
2020-10-19 21:08:57 -0500

    test control values

---
+++ SocialMedia.API/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.API.Application.Logic.Users.Query;
+using SocialMedia.API.Domain.Models;
+
+namespace SocialMedia.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ValuesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+
+        }
+        [HttpGet("GetValue")]
+        public async Task<ActionResult<User>> Login()
+        {
+
+            return Ok("10");
+        }
+    }
+}
